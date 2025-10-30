415f369367a9f525769c36fdabdfcd063cf6c44e
riiz08 <mhmdrizki0812@gmail.com>
2025-10-30 09:42:36 +0700

    set sitempat using edge runtime

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,8 @@ import { MetadataRoute } from "next";
 import { getDramaSitemapData } from "@/app/actions/drama/seo";
 import { getAllEpisodeSlugs } from "@/app/actions/episode";
 
+export const runtime = "edge";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://mangeakkk.my.id";
 
